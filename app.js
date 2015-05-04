d4d4066f67c94b36a7a78af35681bab6bf833677
Kenneth To <kennethkto@gmail.com>
2015-05-04 13:21:49 -0600

    add sound playing, stopping and record spinning

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+//=require timer
+
 $(document).ready(function() {
   $digi1 = document.getElementById("didgi4");
 
   $(".record").on("click", function() {
     $('#stopwatch').timer();
-  })
-})
+
+    $(".record").toggleClass("rotate");
+
+    if ($(".record").hasClass("rotate")===false) {
+      $('#stopwatch').timer('pause');
+    }
+
+
+    if($(".record").hasClass('rotate')) {
+      $('audio')[0].currentTime = 5;
+      $('audio')[0].play();
+
+    }
+    else if($('.record').hasClass('rotate') === false) {
+        $('audio')[0].pause();
+    }
+
+
+  });
+});
